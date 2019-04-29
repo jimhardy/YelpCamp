6417c0b04622d45caa94f561eec9aa140b62e46a
James Hardy <jamesthardy@gmail.com>
2019-04-29 12:26:24 +0000

    container is depreciated. switching to card & card-body

--- seeds.js
+++ seeds.js
@@ -5,17 +5,17 @@ var Comment = require('./models/comment');
 let data = [{
         name: 'Boswell Farms Happy Camp',
         image: 'https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201__340.jpg',
-        description: 'blah blah blah'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt enim in lectus varius, finibus pharetra erat molestie. Mauris eros lacus, tristique eget mauris ac, dapibus vehicula lectus. Praesent placerat purus nulla, nec pellentesque metus gravida a. Proin tempor non enim nec lacinia. Fusce sed aliquet ipsum, quis congue quam. Cras tincidunt dolor vitae est scelerisque, in scelerisque metus pellentesque. Aliquam non dignissim nisl. Suspendisse neque quam, vehicula et elit quis, pretium fermentum metus. Curabitur laoreet, enim a scelerisque laoreet, lorem sem facilisis ante, at consequat ligula nulla nec diam.'
     },
     {
         name: 'Boswell Farms Happy Camp',
         image: 'https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201__340.jpg',
-        description: 'blah blah blah'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt enim in lectus varius, finibus pharetra erat molestie. Mauris eros lacus, tristique eget mauris ac, dapibus vehicula lectus. Praesent placerat purus nulla, nec pellentesque metus gravida a. Proin tempor non enim nec lacinia. Fusce sed aliquet ipsum, quis congue quam. Cras tincidunt dolor vitae est scelerisque, in scelerisque metus pellentesque. Aliquam non dignissim nisl. Suspendisse neque quam, vehicula et elit quis, pretium fermentum metus. Curabitur laoreet, enim a scelerisque laoreet, lorem sem facilisis ante, at consequat ligula nulla nec diam.'
     },
     {
         name: 'Boswell Farms Happy Camp',
         image: 'https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201__340.jpg',
-        description: 'blah blah blah'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt enim in lectus varius, finibus pharetra erat molestie. Mauris eros lacus, tristique eget mauris ac, dapibus vehicula lectus. Praesent placerat purus nulla, nec pellentesque metus gravida a. Proin tempor non enim nec lacinia. Fusce sed aliquet ipsum, quis congue quam. Cras tincidunt dolor vitae est scelerisque, in scelerisque metus pellentesque. Aliquam non dignissim nisl. Suspendisse neque quam, vehicula et elit quis, pretium fermentum metus. Curabitur laoreet, enim a scelerisque laoreet, lorem sem facilisis ante, at consequat ligula nulla nec diam.'
     }
 ]
 
