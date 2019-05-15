270fd8260cb4a12c03b150b69f3948418c3858b2
James Hardy <jamesthardy@gmail.com>
2019-05-15 20:05:27 +0100

    heroku deployment test

--- comment.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const commentSchema = new mongoose.Schema({
-    text: String,
-    author: String
-});
-
-module.exports = mongoose.model('Comment', commentSchema);
