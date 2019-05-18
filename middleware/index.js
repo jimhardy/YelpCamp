cda81306d544203cc840ebb6b4b306a5be9a385a
James Hardy <jamesthardy@gmail.com>
2019-05-18 20:36:32 +0100

    admin can edit & delete all cg & co

--- middleware/index.js
+++ middleware/index.js
@@ -14,7 +14,7 @@ middlewareObj.checkCampgroundOwnership = (req, res, next) => {
 				res.redirect("back");
 			} else {
 				// does user own the campground?
-				if (foundCampground.author.id.equals(req.user._id)) {
+				if (foundCampground.author.id.equals(req.user._id) || req.user.isAdmin) {
 					next();
 				} else {
 					req.flash("error", "You don't have permission to do that");
@@ -37,7 +37,7 @@ middlewareObj.checkCommentOwnership = (req, res, next) => {
 				res.redirect("back");
 			} else {
 				// does user own the comment?
-				if (foundComment.author.id.equals(req.user._id)) {
+				if (foundComment.author.id.equals(req.user._id) || req.user.isAdmin) {
 					next();
 				} else {
 					req.flash("error", "You don't have permission to do that");
